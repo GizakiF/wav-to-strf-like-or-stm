13c75f9a92ba0013a973a961775f4d15716bdf37
GizakiF <aranetagian4221@gmail.com>
2025-03-10 09:39:42 +0800

    modified

--- features.py
+++ features.py
@@ -1,23 +1,29 @@
-'''
+"""
 Copyright (c) Baptiste Caramiaux, Etienne Thoret
 Please cite us if you use this script :)
 All rights reserved
 
 Implementation using a light implementation of the Matlab NLS toolbox by TODO
 
-'''
+"""
+
 import numpy as np
 import math
 from scipy import signal
 import matplotlib.pylab as plt
 import utils
+import pickle
+
+with open("COCHBA.pkl", "rb") as file:
+    loaded_data = pickle.load(file)
 
 
-def spectrum2scaletime(stft, num_channels, num_ch_oct, sr_time, nfft_rate,
-                       nfft_scale, KIND):
-    '''
+def spectrum2scaletime(
+    stft, num_channels, num_ch_oct, sr_time, nfft_rate, nfft_scale, KIND
+):
+    """
     spectrum2scaletime
-    '''
+    """
     lgt_time = stft.shape[0]
     mod_scale = np.zeros((lgt_time, nfft_scale), dtype=complex)
     phase_scale = np.zeros((lgt_time, nfft_scale))
@@ -31,11 +37,12 @@ def spectrum2scaletime(stft, num_channels, num_ch_oct, sr_time, nfft_rate,
     return mod_scale, phase_scale, times, scales
 
 
-def scaletime2scalerate(mod_scale, num_channels, num_ch_oct, sr_time,
-                        nfft_rate, nfft_scale, KIND):
-    '''
+def scaletime2scalerate(
+    mod_scale, num_channels, num_ch_oct, sr_time, nfft_rate, nfft_scale, KIND
+):
+    """
     scaletime2scalerate
-    '''
+    """
     lgt_scale = mod_scale.shape[1]
     scale_rate = np.zeros((nfft_rate, lgt_scale), dtype=complex)
     phase_scale_rate = np.zeros((nfft_rate, lgt_scale))
@@ -48,9 +55,19 @@ def scaletime2scalerate(mod_scale, num_channels, num_ch_oct, sr_time,
     return scale_rate, phase_scale_rate, rates, scales
 
 
-def scalerate2cortical(stft, scaleRate, phase_scale_rate, scales, rates,
-                       num_channels, num_ch_oct, sr_time, nfft_rate,
-                       nfft_scale, KIND):
+def scalerate2cortical(
+    stft,
+    scaleRate,
+    phase_scale_rate,
+    scales,
+    rates,
+    num_channels,
+    num_ch_oct,
+    sr_time,
+    nfft_rate,
+    nfft_scale,
+    KIND,
+):
     LgtRateVector = len(rates)
     LgtScaleVector = len(scales)  # length scale vector
     LgtFreq = stft.shape[1]
@@ -61,69 +78,76 @@ def scalerate2cortical(stft, scaleRate, phase_scale_rate, scales, rates,
     # plt.show()
 
     cortical_rep = np.zeros(
-        (LgtTime, LgtFreq, LgtScaleVector, LgtRateVector), dtype=complex)
+        (LgtTime, LgtFreq, LgtScaleVector, LgtRateVector), dtype=complex
+    )
     for j in range(LgtRateVector):
         fc_rate = rates[j]
         t = np.arange(nfft_rate / 2) / sr_time * abs(fc_rate)
-        h = np.sin(2 * math.pi * t) * np.power(t, 2) * np.exp(
-            -3.5 * t) * abs(fc_rate)
+        h = np.sin(2 * math.pi * t) * np.power(t, 2) * np.exp(-3.5 * t) * abs(fc_rate)
         h = h - np.mean(h)
         STRF_rate0 = np.fft.fft(h, nfft_rate)
-        A = utils.angle(STRF_rate0[:nfft_rate // 2])
+        A = utils.angle(STRF_rate0[: nfft_rate // 2])
         A[0] = 0.0  # instead of pi
-        STRF_rate = np.absolute(STRF_rate0[:nfft_rate // 2])
+        STRF_rate = np.absolute(STRF_rate0[: nfft_rate // 2])
         STRF_rate = STRF_rate / np.max(STRF_rate)
         STRF_rate = STRF_rate * np.exp(1j * A)
         # rate filtering modification
         # STRF_rate                = [STRF_rate(1:nfft_rate/2); zeros(1,nfft_rate/2)']
-        STRF_rate.resize((nfft_rate, ))
+        STRF_rate.resize((nfft_rate,))
         STRF_rate[nfft_rate // 2] = np.absolute(STRF_rate[nfft_rate // 2 + 1])
 
-        if (fc_rate < 0):
+        if fc_rate < 0:
             STRF_rate[1:nfft_rate] = np.matrix.conjugate(
-                np.flipud(STRF_rate[1:nfft_rate]))
+                np.flipud(STRF_rate[1:nfft_rate])
+            )
 
         z1 = np.zeros((nfft_rate, nfft_scale // 2), dtype=complex)
         for m in range(nfft_scale // 2):
-            z1[:, m] = STRF_rate * scaleRate[:, m] * np.exp(
-                1j * phase_scale_rate[:, m])
+            z1[:, m] = STRF_rate * scaleRate[:, m] * np.exp(1j * phase_scale_rate[:, m])
         # z1.resize((nfft_rate,nfft_rate))
         for i in range(nfft_scale // 2):
             z1[:, i] = np.fft.ifft(z1[:, i])
         # print(z1[10,:])
-        
+
         for i in range(LgtScaleVector):
             fc_scale = scales[i]
 
-            R1 = np.arange(nfft_scale / 2) / (
-                nfft_scale / 2) * num_ch_oct / 2 / abs(fc_scale)
+            R1 = (
+                np.arange(nfft_scale / 2)
+                / (nfft_scale / 2)
+                * num_ch_oct
+                / 2
+                / abs(fc_scale)
+            )
             if KIND == 1:
-                C1 = 1 / 2 / .3 / .3
+                C1 = 1 / 2 / 0.3 / 0.3
                 STRF_scale = np.exp(-C1 * np.power(R1 - 1, 2)) + np.exp(
-                    -C1 * np.power(R1 + 1, 2))
+                    -C1 * np.power(R1 + 1, 2)
+                )
             elif KIND == 2:
                 R1 = np.power(R1, 2)
                 STRF_scale = R1 * np.exp(1 - R1)
             z = np.zeros((LgtTime, nfft_scale // 2), dtype=complex)
-           
+
             for n in range(LgtTime):
                 temp = np.fft.ifft(STRF_scale * z1[n, :], nfft_scale)
-                z[n, :] = temp[:nfft_scale // 2]            
+                z[n, :] = temp[: nfft_scale // 2]
             cortical_rep[:, :, i, j] = z[:LgtTime, :LgtFreq]
-    #strf_avg = np.mean(cortical_rep, axis=(0, 1))
-        
+    # strf_avg = np.mean(cortical_rep, axis=(0, 1))
+
     return cortical_rep
 
 
-#### NLS lite
+# NLS lite
 
 
-def waveform2auditoryspectrogram(x_, frame_length, time_constant,
-                                 compression_factor, octave_shift, filt, VERB):
-    '''
+def waveform2auditoryspectrogram(
+    x_, frame_length, time_constant, compression_factor, octave_shift, filt, VERB
+):
+    """
     Wav2Aud form NSL toolbox
     @url http://www.isr.umd.edu/Labs/NSL/Software.htm
-    '''
+    """
 
     # if (filt == 'k'):
     #     raise ValueError('Please use wav2aud_fir function for FIR filtering!')
@@ -136,22 +160,21 @@ def waveform2auditoryspectrogram(x_, frame_length, time_constant,
     # COCHBA = np.asarray(
     #     [[complex(i.replace('i', 'j')) for i in COCHBA[row, :]]
     #      for row in range(len(COCHBA))])
-    COCHBA = utils.COCHBA
+    COCHBA = loaded_data["cochba"]
 
     L, M = COCHBA.shape[0], COCHBA.shape[1]  # p_max = L - 2;
     L_x = len(x_)  # length of input
 
     # octave shift, nonlinear factor, frame length, leaky integration
-    shft = octave_shift  #paras[3]  # octave shift
-    fac = compression_factor  #paras[2]  # nonlinear factor
-    L_frm = round(frame_length * 2**(4 + shft))  # frame length (points)
+    shft = octave_shift  # paras[3]  # octave shift
+    fac = compression_factor  # paras[2]  # nonlinear factor
+    L_frm = round(frame_length * 2 ** (4 + shft))  # frame length (points)
 
-    alph = math.exp(-1 / (time_constant * 2**
-                          (4 + shft))) if time_constant else 0
+    alph = math.exp(-1 / (time_constant * 2 ** (4 + shft))) if time_constant else 0
 
     # hair cell time constant in ms
     haircell_tc = 0.5
-    beta = math.exp(-1 / (haircell_tc * 2**(4 + shft)))
+    beta = math.exp(-1 / (haircell_tc * 2 ** (4 + shft)))
 
     # get data, allocate memory for ouput
     N = math.ceil(L_x / L_frm)
@@ -159,13 +182,13 @@ def waveform2auditoryspectrogram(x_, frame_length, time_constant,
     x.resize((N * L_frm, 1))  # zero-padding
     v5 = np.zeros((N, M - 1))
 
-    #% last channel (highest frequency)
+    # % last channel (highest frequency)
     p = COCHBA[0, M - 1].real
     B = COCHBA[np.arange(int(p) + 1) + 1, M - 1].real
     A = COCHBA[np.arange(int(p) + 1) + 1, M - 1].imag
     y1 = signal.lfilter(B, A, x, axis=0)
     y2 = utils.sigmoid(y1, fac)
-    if (fac != -2):
+    if fac != -2:
         y2 = signal.lfilter([1.0], [1.0, -beta], y2, axis=0)
     y2_h = y2
     # % All other channels
@@ -177,7 +200,8 @@ def waveform2auditoryspectrogram(x_, frame_length, time_constant,
         y1 = signal.lfilter(B, A, x, axis=0)
         y2 = utils.sigmoid(y1, fac)
         # hair cell membrane (low-pass <= 4 kHz) ---> y2 (ignored for linear)
-        if (fac != -2): y2 = signal.lfilter([1.0], [1.0, -beta], y2, axis=0)
+        if fac != -2:
+            y2 = signal.lfilter([1.0], [1.0, -beta], y2, axis=0)
 
         y3 = y2 - y2_h
         y2_h = y2
@@ -188,9 +212,10 @@ def waveform2auditoryspectrogram(x_, frame_length, time_constant,
         if alph:  # leaky integration
             y5 = signal.lfilter([1.0], [1.0, -alph], y4, axis=0)
             v5[:, ch] = y5[L_frm * np.arange(1, N + 1) - 1].reshape(
-                -1, )
+                -1,
+            )
         else:  # short-term average
-            if (L_frm == 1):
+            if L_frm == 1:
                 v5[:, ch] = y4
             else:
                 v5[:, ch] = np.mean(y4.reshape(L_frm, N), axis=0)
@@ -216,8 +241,7 @@ def complexSpectrogram(waveform, windowSize, frameStep):
 
     spectrogram__ = np.zeros((fftSize, frameCount))
     # % h = hamming(windowSize)';
-    h = 0.54 - 0.46 * np.cos(2 * math.pi * np.arange(windowSize) /
-                             (windowSize - 1))
+    h = 0.54 - 0.46 * np.cos(2 * math.pi * np.arange(windowSize) / (windowSize - 1))
     # % h = h * 0 + 1;              % Just for debugging, no window.
 
     # % Note: This code loads the waveform data (times hamming) into the center
